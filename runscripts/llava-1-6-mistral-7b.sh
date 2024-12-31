b5c789373c6d80106b8d213d7d5b492c2aff8599
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2024-12-31 14:15:17 +0900

    runscripts

---
+++ runscripts/llava-1-6-mistral-7b.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#PJM -L rscgrp=share-short
+#PJM -L gpu=1
+#PJM -g gn53
+#PJM -X
+#PJM -j
+module load cuda/12.1
+module load cudnn/8.8.1
+
+cd /work/gn53/k75057/projects/evolutionary-model-merge
+
+source ~/sakana/bin/activate
+python evaluate.py --config_path configs/vlm/llava-1-6-mistral-7b.yaml
+
